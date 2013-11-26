73d2c00918dd4b2e1b796e9b58dac98fe66f6115
kapoorlakshya <lkapoor@mail.csuchico.edu>
2013-11-26 08:52:27 -0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myflicks::Application.config.secret_key_base = 'f215cb97dcba01b3a9d7cda170c54bfae1fefa8e511763edf8acee1d95432f0123e99541ab4d4e752a99723f19965e12775489d134eafc9005b307fadf7c8021'
