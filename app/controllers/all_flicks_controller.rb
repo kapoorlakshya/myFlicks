d69a6df55d73cd88f83e449452b9a97201e0a98a
kapoorlakshya <lkapoor@mail.csuchico.edu>
2013-12-02 15:20:41 -0800

    quick_add bug fixed

--- app/controllers/all_flicks_controller.rb
+++ app/controllers/all_flicks_controller.rb
@@ -3,25 +3,29 @@ class AllFlicksController < ApplicationController
     if !user_signed_in?
       redirect_to root_url, notice: "You must be signed in to access other pages!"
     else
-      @active_flicks = Array.new
-      @not_in_current_user_flicks = Array.new
-      @all_user_flicks = Flick.all
+      all_user_flicks = Flick.all.to_a # All flicks in Flick table
+      @active_flicks_data = Array.new # Store imdb_data of active flicks
 
-      @all_user_flicks.each { |f|
+      @current_user_list = Array.new # Flicks in current user's list
+
+      all_user_flicks.each { |f|
         begin
-          active_flicks_by_users = ImdbDatum.find(f.imdb_id)
+          # Fetch data for active flicks
+          active_flicks_by_users = ImdbDatum.find(f.imdb_id) 
           if active_flicks_by_users
-            @active_flicks.push(active_flicks_by_users)
+            @active_flicks_data.push(active_flicks_by_users) # Populate active_flicks array to print flicks on index
           end
-
-          if f.user_id != current_user.id
-            @not_in_current_user_flicks.push(f.imdb_id)
+      
+          # Show quick_add_button only if user doesn't have the flick in their list
+          if f.user_id == current_user.id
+            @current_user_list.push(active_flicks_by_users.imdb_id)
           end
         rescue
           next
         end
       }
-      @active_flicks.uniq!
+
+      @active_flicks_data.uniq!
     end
   end
 end
